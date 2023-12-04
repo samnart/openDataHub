fbf0e5a7021c72e4c2f8e4e17a9bee4e8468347a
samnart <tettey1234567@gmail.com>
2023-12-04 04:01:06 +0100

    modified the temperature class

--- DataValidator/src/main/java/org/example/api/TemperatureAPI.java
+++ DataValidator/src/main/java/org/example/api/TemperatureAPI.java
@@ -1,17 +1,16 @@
 package org.example.api;
-import com.opendatahub.examples.java.api;
 
 public class TemperatureAPI {
 
     private final String BASE_URL = "https://tourism.opendatahub.com/v1/Weather?";
 
-    private SimpleHttpClient httpClient; // need to resolve this **simpleHttpClient
+//    private SimpleHttpClient httpClient; // need to resolve this **simpleHttpClient
 
-    public TemperatureAPI() {
-        this.httpClient = new SimpleHttpClient(); //
-    }
+//    public TemperatureAPI() {
+//        this.httpClient = new SimpleHttpClient(); //
+//    }
 
-    public String getData() throws Exception {
-        return httpClient.get(BASE_URL + "temperature");
-    }
+//    public String getData() throws Exception {
+//        return httpClient.get(BASE_URL + "temperature");
+//    }
 }
